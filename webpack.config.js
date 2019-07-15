88f5d0c593c02940a0839ed5a8ec2823e49911c3
tangxiaozhuo <tangxiaozhuo001@ke.com>
2019-07-15 18:42:52 +0800

    打包

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, './build'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
     libraryTarget: 'umd',
     globalObject: 'this',
